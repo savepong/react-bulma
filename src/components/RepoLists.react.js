107b189bf9f9971faa33e8821aef3b4083517980
Savepong <savepong.com@gmail.com>
2019-03-20 00:15:22 +0700

    Added state array

--- src/components/RepoLists.react.js
+++ src/components/RepoLists.react.js
@@ -2,12 +2,22 @@ import React from 'react'
 import RepoListItem from './RepoListItem.react'
 
 class RepoLists extends React.Component {
+    
+    state = {
+        repositoryNames: [
+            "facebook/react",
+            "vuejs/vue",
+            "angular/angular"
+        ]
+    }
+
     render() {
+        const repositoryItems = this.state.repositoryNames.map( (name) => {
+            return <RepoListItem repoName={name} key={name} />
+        })
         return (
             <div className="section">
-                <RepoListItem repoName="facebook/react" />
-                <RepoListItem repoName="vuejs/vue"/>
-                <RepoListItem repoName="angular/angular"/>
+                { repositoryItems }
             </div>
         )
     }
