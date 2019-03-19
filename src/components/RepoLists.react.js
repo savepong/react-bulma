59e5b850dc4a12e97c824fc3528547d1c0b7b2c4
Savepong <savepong.com@gmail.com>
2019-03-20 00:10:10 +0700

    Added Repository Loader and API

--- src/components/RepoLists.react.js
+++ src/components/RepoLists.react.js
@@ -5,9 +5,9 @@ class RepoLists extends React.Component {
     render() {
         return (
             <div className="section">
-                <RepoListItem />
-                <RepoListItem />
-                <RepoListItem />
+                <RepoListItem repoName="facebook/react" />
+                <RepoListItem repoName="vuejs/vue"/>
+                <RepoListItem repoName="angular/angular"/>
             </div>
         )
     }
